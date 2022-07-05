7d362e3a0406652854c01e7c50c6863049fd728c
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-05 19:16:52 +0930

    4.2 Blog list, step2

---
+++ utils/middleware.js
@@ -0,0 +1,28 @@
+const morgan = require('morgan')
+const logger = require('./logger')
+
+morgan.token('body', (request) => JSON.stringify(request.body))
+
+const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
