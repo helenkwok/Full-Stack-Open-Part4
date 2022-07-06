f2e1f02ca4d944f2a5e9909ead4a05aec9e01f6d
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-06 12:56:58 +0930

    4.10: Blog list tests, step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -60,13 +60,44 @@ test('blogs are returned as json', async () => {
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-
   expect(response.body).toHaveLength(initialBlogs.length)
 })
 
-test('The unique identifier property of the blog posts is id, which is named as _id by default', async () => {
+test('unique identifier property of the blog posts is id, which is named as _id by default', async () => {
   const response = await api.get('/api/blogs')
   expect(JSON.parse(response.text)[0].id).toBeDefined()
+})
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+  const title = initialBlogs[Math.floor(Math.random()*initialBlogs.length)].title
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain(title)
+})
+
+test('a new blog is successfully created', async () => {
+  const newBlog = {
+    title: 'Dummy Blog',
+    author: 'Anon',
+    url: 'http://blog.dummy.com',
+    likes: 1,
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+
+  const storedBlog = response.body[response.body.length - 1]
+  expect({
+    title: storedBlog.title,
+    author: storedBlog.author,
+    url: storedBlog.url,
+    likes: storedBlog.likes
+  }).toEqual(newBlog)
 })
 
 afterAll(() => {
