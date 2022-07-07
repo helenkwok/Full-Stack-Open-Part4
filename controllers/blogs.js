6c316842dfe575097e1968b9bad397a26b29f866
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-07 21:54:35 +0930

    4.19: bloglist expansion, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -17,14 +18,28 @@ blogsRouter.get('/:id', async (request, response) => {
     response.status(404).end()
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
-  const user = await User.findById(request.body.userId)
+  const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes || 0,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
     user: user._id
   })
 
@@ -38,10 +53,11 @@ blogsRouter.post('/', async (request, response) => {
 
 blogsRouter.delete('/:id', async (request, response) => {
   const result = await Blog.findByIdAndRemove(request.params.id)
-  result ?
+  if (result) {
     response.status(204).end()
-    :
+  } else {
     response.status(400).end()
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
